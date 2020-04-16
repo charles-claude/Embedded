0a3009caa101cf3f16741dcce00105b73ffb6cba
charles <charles.claude@epitech.eu>
2020-04-16 17:39:00 +0200

    test ca

--- static/script.js
+++ static/script.js
@@ -1,10 +1,10 @@
 
-$(".update").click(function(){
+$("#update").click(function(){
     var repeat = document.getElementById("repeat").checked;
     var DataJson = '{ "Repeat" : '+ repeat + '}'
-    
+
     $.ajax({
-        url: "/config",
+        url: "127.0.0.1/config",
         method: "POST",
         data: DataJson,
         dataType : "json",
