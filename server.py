56e09f14048ace137c00864a3fb96663dc88eabd
louison.harizi <louison.harizi@epitech.eu>
2020-04-16 17:15:29 +0200

    restart

--- server.py
+++ server.py
@@ -1,8 +1,27 @@
 
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/config', methods=['GET', 'POST'])
+def config():
+
+    if (request.method == 'GET'):
+        if (!os.path.exists(file_name):
+            return ({})
+        else:
+            f = open("bjtubot.conf", "r")
+            return (f.read())
+        
+    elif (request.method == 'POST'):
+        data = request.form
+        f = open("bjtubot.conf", "w")
+        f.write(data)
+        f.close()
+        command = os.popen('./run.sh restart')
+    return ""
