f34b7fb7a31bc171d01c9366d9b7530c68d1c33c
louison.harizi <louison.harizi@epitech.eu>
2020-04-16 15:20:14 +0200

    run

--- run.sh
+++ run.sh
@@ -2,7 +2,26 @@
 
 export FLASK_APP=server.py
 
-docker-compose up --build &
-echo $! >>/tmp/bjtubot_docker.pid
 
-flask run
+
+
+startme() {
+    docker-compose up --build &
+    echo $! >>bjtubot_docker.pid
+    flask run &
+    echo $! >>bjtubot_server.pid
+}
+
+stopme() {
+    kill $(cat njtubot_docker.pid)
+    kill $(cat njtubot_server.pid)
+}
+
+case "$1" in
+    start)   startme ;;
+    stop)    stopme ;;
+    restart) stopme; startme ;;
+    *) echo "usage: $0 start|stop|restart" >&2
+       exit 1
+       ;;
+esac
