8c384eb8e3b33cfeabc5f434f3ea0e0e8cf6da29
charles <charles.claude@epitech.eu>
2020-04-17 16:21:59 +0200

    add more parameters

--- static/script.js
+++ static/script.js
@@ -1,11 +1,18 @@
 
 $("#update").click(function(){
-    var repeat = document.getElementById("repeat").checked;
-    var bannword = document.getElementById("bannword").value
-    var bannlist = bannword.split(',')
-    var DataJson = { Repeat :  repeat , Bannword : bannlist}
-    var DataString = JSON.stringify(DataJson)
+    var repeat = document.getElementById("repeat").checked
+    var slow = document.getElementById("slow").checked
+    var unique = document.getElementById("unique").checked
+    var slowduration = document.getElementById("slowduration").value
+    var botcolor = document.getElementById("colorpicker").value
+    var samecolor = document.getElementById("samecolor").checked
 
+    var bannword = document.getElementById("bword").value
+    var bannlist = bannword.split(',')
+    var DataJson = { Repeat :  repeat , Bannword : bannlist, Slow: slow, Unique: unique, Slowduration: slowduration, Color: botcolor, Samecolor: samecolor}
+
+    var DataString = JSON.stringify(DataJson)
+    console.log(DataString)
 
     $.ajax({
         url: "/config",
